11e7032393dc7d31488e63c600de2575a64f3850
Majak Deng <majakbanks.deng>
2024-07-25 14:23:37 +0300

    modified

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+
+// Step 1: Declare companyName as a constant
+// We use 'const' because the company name shouldn't change
+// The test specifically checks that we're using 'const' here
+const companyName = 'Scuber';
+
+// Step 2: Declare mostProfitableNeighborhood using 'let'
+// We use 'let' because the most profitable neighborhood could change over time
+// The test checks that we're using 'let' and that the initial value is 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Step 3: Declare companyCeo using 'let'
+// Similar to mostProfitableNeighborhood, we use 'let' because the CEO could change
+// The test verifies we're using 'let' and that the initial value is 'Susan Smith'
+let companyCeo = 'Susan Smith';
+
+// These declarations satisfy all the requirements specified in the tests:
+// 1. companyName is declared with 'const' and set to 'Scuber'
+// 2. mostProfitableNeighborhood is declared with 'let' and set to 'Chelsea'
+// 3. companyCeo is declared with 'let' and set to 'Susan Smith'n
